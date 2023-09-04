9199b2e0a7a1a12b7e15d3f64aa93b1976d3eb65
Albin Dahlén <albin@dahlen.ws>
2023-09-04 16:51:36 +0200

    Created a random number generator

--- Workout_logger/app/src/main/java/com/example/workout_logger/MainActivity.java
+++ Workout_logger/app/src/main/java/com/example/workout_logger/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.workout_logger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button num_gen_button = findViewById(R.id.generator_button);
+        TextView generated_num_text = findViewById(R.id.generated_number);
+        EditText limit = findViewById(R.id.limit);
+
+        num_gen_button.setOnClickListener(view -> {
+            int number = 100;
+            try {
+                number = Integer.parseInt(limit.getText().toString());
+            } catch (NumberFormatException e) {
+                // We get exception continue with default value
+            }
+            generated_num_text.setText(Integer.toString(generateNumber(number)));
+        });
+
+    }
+
+    private int generateNumber(int limit) {
+        Random rand = new Random();
+
+        return rand.nextInt(limit);
     }
 }
