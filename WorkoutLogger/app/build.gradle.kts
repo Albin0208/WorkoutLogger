e25167c50b36b9847abf721f1ff258c5ff162f97
Albin Dahlén <albin@dahlen.ws>
2023-10-14 13:16:09 +0200

    Added navigation

--- WorkoutLogger/app/build.gradle.kts
+++ WorkoutLogger/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.workoutlogger"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.workoutlogger"
@@ -35,9 +35,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.1.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.4")
+    implementation("androidx.navigation:navigation-ui:2.7.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/MainActivity.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.workoutlogger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,23 +30,28 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     FirebaseUser user;
 
-    private RecyclerView recyclerView;
-    private ExerciseAdapter exerciseAdapter;
-
+//    private RecyclerView recyclerView;
+//    private ExerciseAdapter exerciseAdapter;
+        BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> exercises = new ArrayList<>(Arrays.asList("Knäböj", "Bänkpress", "Marklyft"));
-
-        recyclerView = findViewById(R.id.recycler_view);
-        exerciseAdapter = new ExerciseAdapter(exercises);
-        recyclerView.setAdapter(exerciseAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-
         auth = FirebaseAuth.getInstance();
-        btn = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_menu);
+        NavController navController = Navigation.findNavController(this, R.id.host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+//        recyclerView = findViewById(R.id.recycler_view);
+//        exerciseAdapter = new ExerciseAdapter(exercises);
+//        recyclerView.setAdapter(exerciseAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//
+//        btn = findViewById(R.id.logout);
+//        textView = findViewById(R.id.user_details);
 
         user = auth.getCurrentUser();
 
@@ -48,14 +60,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         } else {
-            textView.setText(user.getEmail());
+//            textView.setText(user.getEmail());
         }
 
-        btn.setOnClickListener(View -> {
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        });
+//        btn.setOnClickListener(View -> {
+//            FirebaseAuth.getInstance().signOut();
+//            Intent intent = new Intent(getApplicationContext(), Login.class);
+//            startActivity(intent);
+//            finish();
+//        });
     }
 }
