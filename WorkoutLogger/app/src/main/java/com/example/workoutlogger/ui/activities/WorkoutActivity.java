13bfde4ece0f6957457d431ea8f6313682cb21a8
Albin Dahlén <albin@dahlen.ws>
2023-10-22 18:36:45 +0200

    Extracted dialog creation to own method

--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/WorkoutActivity.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/WorkoutActivity.java
@@ -1,16 +1,13 @@
 package com.example.workoutlogger.ui.activities;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
@@ -24,7 +21,6 @@ import com.example.workoutlogger.ui.adapters.WorkoutAdapter;
 import com.example.workoutlogger.viewmodels.WorkoutViewModel;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class WorkoutActivity extends AppCompatActivity {
@@ -37,7 +33,6 @@ public class WorkoutActivity extends AppCompatActivity {
                 if (result.getResultCode() == RESULT_OK && result.getData() != null) {
                     Exercise exercise = result.getData().getParcelableExtra("exercise", Exercise.class);
                     workoutViewModel.addExercise(exercise);
-
                 }
             });
     @Override
@@ -61,7 +56,9 @@ public class WorkoutActivity extends AppCompatActivity {
         workoutViewModel.getExercises().observe(this, exercises -> {
             workoutAdapter.submitList(new ArrayList<>(exercises));
         });
+
         Button addExerciseButton = findViewById(R.id.add_exercise_button);
+
         addExerciseButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, ChooseExerciseActivity.class);
             chooseExerciseLauncher.launch(intent);
@@ -71,34 +68,38 @@ public class WorkoutActivity extends AppCompatActivity {
         ImageButton abortWorkout = view.findViewById(R.id.abort_workout);
         ImageButton finishWorkout = view.findViewById(R.id.finish_workout);
 
-        abortWorkout.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            final View customLayout = LayoutInflater.from(this).inflate(R.layout.dialog_abort_workout, null);
-
-            Button yesButton = customLayout.findViewById(R.id.confirm_button);
-            Button noButton = customLayout.findViewById(R.id.cancel_button);
-
-            builder.setView(customLayout);
-            AlertDialog dialog = builder.create();
-
-            // Apply the custom background to the dialog's window
-            Window window = dialog.getWindow();
-            if (window != null) {
-                window.setBackgroundDrawableResource(R.drawable.rounded_dialog_bg); // Reference to your custom background drawable
-            }
-
-            yesButton.setOnClickListener(v1 -> finish());
-
-            noButton.setOnClickListener(v1 -> dialog.dismiss());
-
-
-            dialog.show();
-        });
-
+        abortWorkout.setOnClickListener(v -> showAbortWorkout());
 
         finishWorkout.setOnClickListener(v -> {
             // TODO Implement finishing the workout
         });
+    }
+
+    /**
+     * Shows a dialog asking the user if they want to abort the workout
+     */
+    private void showAbortWorkout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final View customLayout = LayoutInflater.from(this).inflate(R.layout.dialog_abort_workout, null);
+
+        Button yesButton = customLayout.findViewById(R.id.confirm_button);
+        Button noButton = customLayout.findViewById(R.id.cancel_button);
+
+        builder.setView(customLayout);
+        AlertDialog dialog = builder.create();
+
+        // Apply the custom background to the dialog's window
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setBackgroundDrawableResource(R.drawable.rounded_dialog_bg); // Reference to your custom background drawable
+        }
+
+        yesButton.setOnClickListener(v1 -> finish());
+
+        noButton.setOnClickListener(v1 -> dialog.dismiss());
+
+
+        dialog.show();
     }
 }
