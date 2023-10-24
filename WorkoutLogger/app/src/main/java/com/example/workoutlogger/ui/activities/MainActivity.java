f6b51a333aa96cfafdd0a512f0958df133942376
Albin Dahlén <albin@dahlen.ws>
2023-10-24 15:47:26 +0200

    Changed main to use authViewModel

--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/MainActivity.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/MainActivity.java
@@ -4,25 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.workoutlogger.R;
-import com.example.workoutlogger.viewmodels.UserViewModel;
+import com.example.workoutlogger.viewmodels.AuthViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    private final UserViewModel userViewModel = new UserViewModel();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AuthViewModel authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+
         // Check if logged in
-        if (!userViewModel.isUserSignedIn()) {
+        if (!authViewModel.isUserSignedIn()) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
             finish();
