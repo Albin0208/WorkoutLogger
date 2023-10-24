1c6d05a21dd0d30a3eb3c02b782131c56ebad7db
Albin Dahlén <albin@dahlen.ws>
2023-10-24 15:45:03 +0200

    Changed signout to use auth

--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/fragments/SettingsFragment.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/fragments/SettingsFragment.java
@@ -8,26 +8,27 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.workoutlogger.R;
 import com.example.workoutlogger.ui.activities.LoginActivity;
-import com.example.workoutlogger.viewmodels.UserViewModel;
+import com.example.workoutlogger.viewmodels.AuthViewModel;
 
 
 public class SettingsFragment extends Fragment {
-    private UserViewModel userViewModel;
+    private AuthViewModel authViewModel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.fragment_settings, container, false);
 
-        userViewModel = new UserViewModel();
+        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
 
         Button btnLogout = view.findViewById(R.id.logout_button);
 
         btnLogout.setOnClickListener(v -> {
-            userViewModel.signOut();
+            authViewModel.signOut();
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
             requireActivity().finish();
