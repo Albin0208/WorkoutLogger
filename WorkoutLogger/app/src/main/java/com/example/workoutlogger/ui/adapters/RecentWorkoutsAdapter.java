7a3c0c6f9972852004310967094e6b1df1587220
Albin Dahlén <albin@dahlen.ws>
2023-11-12 13:58:35 +0100

    Added a last workout item which is gonna redirect the user to the full workout log

--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/adapters/RecentWorkoutsAdapter.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/adapters/RecentWorkoutsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.workoutlogger.ui.adapters;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,25 +20,41 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
-public class RecentWorkoutsAdapter extends RecyclerView.Adapter<RecentWorkoutsAdapter.RecentWorkoutsViewHolder>{
+public class RecentWorkoutsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int VIEW_TYPE_NORMAL = 0;
+    private static final int VIEW_TYPE_LAST = 1;
     private List<Workout> workouts = new ArrayList<>();
 
     @NonNull
     @Override
-    public RecentWorkoutsAdapter.RecentWorkoutsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_LAST) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.workout_item_last, parent, false);
+            return new LastItemViewHolder(view);
+        }
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.workout_item, parent, false);
         return new RecentWorkoutsViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecentWorkoutsAdapter.RecentWorkoutsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof LastItemViewHolder) {
+            return;
+        }
         Workout workout = workouts.get(position);
-        holder.bind(workout);
+        RecentWorkoutsViewHolder workoutsViewHolder = (RecentWorkoutsViewHolder) holder;
+        workoutsViewHolder.bind(workout);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == getItemCount() - 1 ? VIEW_TYPE_LAST : VIEW_TYPE_NORMAL;
     }
 
     @Override
     public int getItemCount() {
-        return workouts.size();
+        return workouts.size() + 1; // Special case for the last item
     }
 
     public void setWorkouts(List<Workout> workouts) {
@@ -45,6 +62,9 @@ public class RecentWorkoutsAdapter extends RecyclerView.Adapter<RecentWorkoutsAd
         notifyDataSetChanged();
     }
 
+    /**
+     * View holder for a normal item in the list
+     */
     public static class RecentWorkoutsViewHolder extends RecyclerView.ViewHolder {
         private final TextView workoutName;
         private final TextView workoutDate;
@@ -61,16 +81,24 @@ public class RecentWorkoutsAdapter extends RecyclerView.Adapter<RecentWorkoutsAd
         public void bind(Workout workout) {
             workoutName.setText(workout.getName());
 
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            SimpleDateFormat formatter = new SimpleDateFormat(
+                    itemView.getContext().getString(R.string.date_format), Locale.getDefault());
             workoutDate.setText(formatter.format(workout.getDate().toDate()));
 
             // Set the text of the exercises to be a comma separated list of all the exercises
-            exercises.setText(
-                workout.getExercises().stream()
+            exercises.setText(String.join(",", workout.getExercises().stream()
                     .map(Exercise::getName)
-                    .collect(Collectors.joining(", "))
-            );
+                    .toArray(String[]::new)));
+        }
+    }
 
+    /**
+     * View holder for the last item in the list
+     * This item is special since it will be used to navigate to the view all workouts screen
+     */
+    public static class LastItemViewHolder extends RecyclerView.ViewHolder {
+        public LastItemViewHolder(@NonNull View itemView) {
+            super(itemView);
         }
     }
 }
