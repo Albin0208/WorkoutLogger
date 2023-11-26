6c9e3cba543e68b88fa050ce8c561c9aeb20180b
Albin Dahlén <albin@dahlen.ws>
2023-11-26 12:25:07 +0100

    Changed how navcontroller is setup to follow dev guide

--- WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/MainActivity.java
+++ WorkoutLogger/app/src/main/java/com/example/workoutlogger/ui/activities/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.workoutlogger.R;
@@ -32,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         bottomNavigationView = findViewById(R.id.bottom_nav_menu);
-        NavController navController = Navigation.findNavController(this, R.id.host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        NavController navController = navHostFragment.getNavController();
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
     }
 }
